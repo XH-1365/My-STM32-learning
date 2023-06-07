ec86a339c7cc62a8f1279eea59d498b737ea0990
HZD <2684791179@qq.com>
2023-06-07 22:32:57 +0800

    上传

--- User/main.c
+++ User/main.c
@@ -2,6 +2,13 @@
 #include "bsp_led.h"
 #include "bsp.key.h"
 
+void Delay500us()		//@11.0592MHz
+{
+	unsigned char i;
+
+	i = 227;
+	while (--i);
+}
 
 int main(void)
 {
@@ -13,8 +20,8 @@ int main(void)
 	{
 		if(Key_Scan(Key_GPIO_PORT,Key_Pin_0) == KEY_ON)
 		{
+			Delay500us();
 			LED_R_TOGGLE;
 		}
 	}
 }
-
